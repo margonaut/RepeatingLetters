c4fc31c321df1f0e4e76feaba33c033da74b7b55
Margaret Morgan <mmorgan@datto.com>
2016-06-24 17:25:45 -0400

    Reordered methods

--- program.rb
+++ program.rb
@@ -1,26 +1,7 @@
-def letter_count(word)
-  results = Hash.new(0)
-  letters = word.split("")
-  letters.each do |letter|
-    results[letter] += 1
-  end
-  results
-end
-
-def most_repeated_letter(word)
-  letters = letter_count(word)
-  most_repeated_letter = letters.max_by { |k, v| v }
-end
-
-def analyze_words(text)
-  words = text.split(' ')
-  results = {}
-  
-  words.each do |word|
-    results[word] = most_repeated_letter(word)
-  end
-  
-  results
+def read_from_file(filename)
+  file = File.open(filename, "r")
+  original_text = file.read
+  text = original_text.downcase.gsub(/[^a-z0-9\s]/i, '')
 end
 
 def sentence_analyzer(text)
@@ -30,10 +11,27 @@ def sentence_analyzer(text)
   puts "The letter #{winning_word[1][0]} is repeated #{winning_word[1][1]} times."
 end
 
-def read_from_file(filename)
-  file = File.open(filename, "r")
-  original_text = file.read
-  text = original_text.downcase.gsub(/[^a-z0-9\s]/i, '')
+def analyze_words(text)
+  words = text.split(' ')
+  results = {}
+  words.each do |word|
+    results[word] = most_repeated_letter(word)
+  end
+  results
+end
+
+def most_repeated_letter(word)
+  letters = letter_count(word)
+  most_repeated_letter = letters.max_by { |k, v| v }
+end
+
+def letter_count(word)
+  results = Hash.new(0)
+  letters = word.split("")
+  letters.each do |letter|
+    results[letter] += 1
+  end
+  results
 end
 
 # User Interaction
