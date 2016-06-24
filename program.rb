dacc484aad18ce7ce6c3aec7bcdf499e495a16e1
Margaret Morgan <mmorgan@datto.com>
2016-06-24 17:08:14 -0400

    Program now checks for .txt in given path

--- program.rb
+++ program.rb
@@ -1,16 +1,5 @@
 require 'pry'
 
-puts "Please enter the name of the file you wish to scan"
-filename = gets.strip
-
-file = File.open(filename, "r")
-
-# file = File.open("example.txt", "r")
-
-original_text = file.read
-text = original_text.downcase.gsub(/[^a-z0-9\s]/i, '')
-
-
 def letter_count(word)
   results = Hash.new(0)
   letters = word.split("")
@@ -20,22 +9,39 @@ def letter_count(word)
   results
 end
 
-def get_most_reapeated_letter(word)
+def get_most_repeated_letter(word)
   letters = letter_count(word)
-  most_reapeated_letter = letters.max_by { |k, v| v }
+  most_repeated_letter = letters.max_by { |k, v| v }
 end
 
 def sentence_analyzer(text)
   words = text.split(' ')
-  words_with_most_reapeated_letter = {}
+  words_with_most_repeated_letter = {}
   
   words.each do |word|
-    words_with_most_reapeated_letter[word] = get_most_reapeated_letter(word)
+    words_with_most_repeated_letter[word] = get_most_repeated_letter(word)
   end
   
-  winning_word = words_with_most_reapeated_letter.max_by {|k, v| v[1] }
+  winning_word = words_with_most_repeated_letter.max_by {|k, v| v[1] }
   
   puts "The winning word is \"#{winning_word[0]},\" with the letter #{winning_word[1][0]} repeated #{winning_word[1][1]} times."
 end
+
+# User Interaction
+
+puts "This program will identify the word in your text file with the most frequently repeated letter"
+puts "Please enter the name or path of the file you wish to scan"
+puts "For example, 'example.txt'"
+
+filename = gets.strip
+
+until filename.include?(".txt")
+  puts "Please make sure you are entering a file name or path ending with .txt"
+  filename = gets.strip
+end
+
+file = File.open(filename, "r")
+original_text = file.read
+text = original_text.downcase.gsub(/[^a-z0-9\s]/i, '')
 
 sentence_analyzer(text)
