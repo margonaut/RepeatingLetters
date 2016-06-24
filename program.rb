6673da1399b8d2872d39335c152aadcbe0be677c
Margaret Morgan <mmorgan@datto.com>
2016-06-24 16:57:26 -0400

    Reorganized some code and renamed some methods

--- program.rb
+++ program.rb
@@ -1,13 +1,13 @@
 require 'pry'
 
-# puts "Please enter the name of the file you wish to scan"
-# filename = gets.strip
+puts "Please enter the name of the file you wish to scan"
+filename = gets.strip
 
-# file = File.open(filename, "r")
+file = File.open(filename, "r")
 
-file = File.open("example.txt", "r")
+# file = File.open("example.txt", "r")
+
 original_text = file.read
-
 text = original_text.downcase.gsub(/[^a-z0-9\s]/i, '')
 
 
@@ -20,26 +20,22 @@ def letter_count(word)
   results
 end
 
+def get_most_reapeated_letter(word)
+  letters = letter_count(word)
+  most_reapeated_letter = letters.max_by { |k, v| v }
+end
 
 def sentence_analyzer(text)
   words = text.split(' ')
-  words_with_highest_letter = {}
+  words_with_most_reapeated_letter = {}
   
   words.each do |word|
-    letters = letter_count(word)
-    highest_letter = letters.max_by { |k, v| v }
-    words_with_highest_letter[word] = highest_letter
+    words_with_most_reapeated_letter[word] = get_most_reapeated_letter(word)
   end
   
-  winning_word = words_with_highest_letter.max_by {|k, v| v[1] }
+  winning_word = words_with_most_reapeated_letter.max_by {|k, v| v[1] }
   
   puts "The winning word is \"#{winning_word[0]},\" with the letter #{winning_word[1][0]} repeated #{winning_word[1][1]} times."
 end
 
 sentence_analyzer(text)
-
-
-
-# what if the file doesn't exist
-# what if it doesn't have txt extension
-# what if a word has multiple letters repeated equal #s of times
