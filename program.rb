85ea2083d8a29b06cee76cc318d11f52f34f9718
Margaret Morgan <mmorgan@datto.com>
2016-06-24 16:07:59 -0400

    Added good character replacement regex

--- program.rb
+++ program.rb
@@ -3,7 +3,9 @@ require 'pry'
 # puts "Please enter the name of the file you wish to scan"
 # filename = gets.strip
 
-text_file = File.open("example.txt", "r")
-contents = text_file.read
+file = File.open("example.txt", "r")
+original_text = file.read
+
+text = original_text.downcase.gsub(/[^a-z0-9\s]/i, '')
 
 binding.pry
