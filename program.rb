b7fc01567fea60c04b3f291e2ab72f0dac728bac
Margaret Morgan <mmorgan@datto.com>
2016-06-24 16:13:02 -0400

    Temporarily removed filename prompter, created words array

--- program.rb
+++ program.rb
@@ -1,11 +1,14 @@
 require 'pry'
 
-puts "Please enter the name of the file you wish to scan"
-filename = gets.strip
+# puts "Please enter the name of the file you wish to scan"
+# filename = gets.strip
 
-file = File.open(filename, "r")
+# file = File.open(filename, "r")
+
+file = File.open("example.txt", "r")
 original_text = file.read
 
 text = original_text.downcase.gsub(/[^a-z0-9\s]/i, '')
+words = text.split(' ')
 
 binding.pry
