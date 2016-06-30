a9c33134aa6153d76bfdb132afb4c09b92f2a35b
Margaret Morgan <mmorgan@datto.com>
2016-06-29 21:14:53 -0400

    Code cleanup and comment adding

--- new_program.rb
+++ new_program.rb
@@ -1,46 +1,22 @@
-# First, extract the test from the file
+require 'pry'
+
 def read_from_file(filename)
+  # First extract the text from the file
   file = File.open(filename, "r")
   text = file.read
-  stripped_text = text.downcase.gsub(/[^a-z0-9\s]/i, '')
   
-  # stop the program if the file is blank or full of unusable characters
+  # strip the text down to usable characters only
+  stripped_text = text.downcase.gsub(/[^a-z0-9\s]/i, '')
+  # and check to make sure the file contains viable content
+  # before proceeding, abort if not
   unless stripped_text.length > 0
     abort("This file contains no suitable text")
   end
   
+  # Return the original text since we want to maintain formatting
+  # for our final output
   text
 end
-
-# def find_winning_word(text)
-#   analyzed_words = analyze_words(text)
-#   winning_word = analyzed_words.max_by {|k, v| v[1] }
-#   puts "The winning word is \"#{winning_word[0]}\""
-#   puts "The letter #{winning_word[1][0]} is repeated #{winning_word[1][1]} times."
-# end
-# 
-# def analyze_words(text)
-#   words = text.split(' ')
-#   results = {}
-#   words.each do |word|
-#     results[word] = most_repeated_letter(word)
-#   end
-#   results
-# end
-# 
-# def most_repeated_letter(word)
-#   letters = letter_count(word)
-#   most_repeated_letter = letters.max_by { |k, v| v }
-# end
-# 
-# def letter_count(word)
-#   results = Hash.new(0)
-#   letters = word.split("")
-#   letters.each do |letter|
-#     results[letter] += 1
-#   end
-#   results
-# end
 
 def get_file
   puts "Please enter the name or path of the file you wish to scan."
@@ -50,7 +26,7 @@ def get_file
   # sure we're getting only the string and not the /n enter character
   filename = gets.strip
   
-  # Our program is looking for a text file. We can perform a First
+  # Our program is looking for a text file. We can perform a first
   # basic check by making sure the correct file extension is included,
   # and prompt for input again if not
   until filename.include?(".txt")
@@ -67,7 +43,7 @@ def score_word(word)
 end
 
 def has_repeated_letters?(word)
-  word.length != word.strip('').uniq.length
+  word.length != word.split('').uniq.length
 end
 
 def get_winning_word(text)
@@ -76,9 +52,9 @@ def get_winning_word(text)
   
   # We're only going to maintain the word in the first position
   # along with its score
-  winning_word = null
+  winning_word = 'doop'
   #example: {text: "aaagh", score: 3}
-  
+  binding.pry
   words.each do |word|
     # We want to keep the original text handy for our final output,
     # so we'll strip extraneous characters out case by case
@@ -86,12 +62,12 @@ def get_winning_word(text)
     stripped_word = word.downcase.gsub(/[^a-z0-9\s]/i, '')
     
     # Let's make sure there are repeated characters
-    # in our word before we continue
-    binding.pry
+    # present in our word before we continue
     if has_repeated_letters?(stripped_word)
       # get score, return integer
       score = score_word(stripped_word)
-      # Compare our score to 
+      # Compare this score with that
+      # of the current winning word
       if score > winning_word[:score]
         winning_word = { text: word, score: score }
       end
