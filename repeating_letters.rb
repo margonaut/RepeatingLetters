2caa41b6d2e8624b8a83224ea674fe5f88534b1f
Margaret Morgan <mmorgan@datto.com>
2016-06-30 05:43:39 -0400

    Comment cleanup

--- repeating_letters.rb
+++ repeating_letters.rb
@@ -4,14 +4,9 @@ require_relative 'repeating_letters_logic'
 puts "This program will identify the word in your text file"
 puts "with the most frequently repeated letter"
 
-# This method prompts the user for a text file path
-# and allows them to proceed only once a valid file is
-# successfully opened
+# Prompt the user for a valid text file path
 filename = get_file_name
 
 if filename
-  # If a file object is present, run program logic
   run_repeating_letters(filename)
 end
-
-# Add unit tests -> helps identify errors, edge cases
