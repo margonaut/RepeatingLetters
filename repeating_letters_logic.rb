07ed74ad57e7504a260c0a094fcc330e09027059
Margaret Morgan <mmorgan@datto.com>
2016-06-30 14:10:09 -0400

    commenting out unused tests

--- repeating_letters_logic.rb
+++ repeating_letters_logic.rb
@@ -51,107 +51,25 @@ def run_repeating_letters(filename)
   end
 end
 
-def open_file(filename)
-  # We want to return a File object if the given path leads to an
-  # existing file. If not, we catch the error and exit the program
-  begin
-    file = File.open(filename, "r")
-  rescue Errno::ENOENT => e
-    puts "No such file."
-  end
-end
 
-def read_text_from_file(file)
-  # First, extract the text from our file object
-  text = file.read
-  File.open(filename, "r") do |f|
-  
-  # strip the text down to usable characters only
-  stripped_text = text.downcase.gsub(/[^a-z0-9\s]/i, '')
-  # and check to make sure this text contains at least two
-  # viable characters before proceeding. Text with a single
-  # character could contain no duplicates and does not
-  # meet our criteria
-  unless stripped_text.length >= 2
-    abort("Invalid content")
-  end
-  
-  # Return the original text for now since we want to maintain formatting
-  # for our final output
-  text
-end
-
-# def get_winning_word(text)
-  # We're only going to maintain the word in the winning position
-  # along with the corresponding score
-  # winning_word = nil
-  # winning_score = 0
-  
-  # Break the text down into individual words
-  # words = text.split(' ')
-  
-  # Sort by the length of the words with extra characters removed, longest first.
-  # words = words.reverse.sort_by{ |a| a.gsub(/[^a-z\s]/i, '').length }.reverse
- #  # 
- # words_and_lengths = words.map{|w|score_word(w)}.reduce Hash.new, :merge
- # max = words_and_lengths.keys.max
- # winning_word = words_and_lengths[max]
- # winning_score = max
-  
-  
-  # words.each do |word|
-  #   # We can greatly reduce runtime at scale by eliminating a big chunk
-  #   # of short words. Because we maintain the winning_score and have sorted
-  #   # our words array by length, we know that the winning_score will be
-  #   # impossible to beat once we reach words with a length less than the current 
-  #   # winning_score. At that point we can disregard the rest of the words.
-  #   
-  #   
-  #   if word.length > winning_score
-  #     
-  #     # We want to keep the original word formatting handy for our final output,
-  #     # but we'll need to score our words based on a version cleaned up with
-  #     # a regex pattern.
-  #     stripped_word = word.downcase.gsub(/[^a-z\s]/i, '')
-  #     
-  #     # Let's make sure there are repeated characters
-  #     # present in our word before we continue. Otherwise,
-  #     # our program will move on to the next word
-  #     if has_repeated_letters?(stripped_word)
-  #       
-  #       # now that we know we're working with a word
-  #       # that contains repeated letters, let's evaluate it for
-  #       # a score: the highest number of times a letter is repeated
-  #       score = score_word(stripped_word)
-  #       
-  #       # Compare this score with that of the current winning_word.
-  #       # Since the first word with the highest score is the one
-  #       # we want to return, we'll only replace our winning_word
-  #       # if we have a score that is greater, not just equal to
-  #       # the current winner
-  #       if score > winning_score
-  #         winning_score = score
-  #         winning_word = word
-  #       end
-  #     end  
-  #   end
-  # end
-  
-  
-  # If our text file ends up containing no words with repeating letters,
-  # the default winning_word will never be overwritten
-  if winning_word
-    # Simply returning the winning string instead of descriptive text 
-    # will make integration into our test suite easier. A terminal output
-    # is included here for human readability
-    puts winning_word
-    winning_word
-  else
-    # If a winning_word does not exist, we know we have not found
-    # any words with repeating letters in the given text
-    puts "No repeating letters."
-  end
-end
+# def read_text_from_file(file)
+#   # First, extract the text from our file object
+#   text = file.read
+#   File.open(filename, "r") do |f|
+#   
+#   # strip the text down to usable characters only
+#   stripped_text = text.downcase.gsub(/[^a-z0-9\s]/i, '')
+#   # and check to make sure this text contains at least two
+#   # viable characters before proceeding. Text with a single
+#   # character could contain no duplicates and does not
+#   # meet our criteria
+#   unless stripped_text.length >= 2
+#     abort("Invalid content")
+#   end
+#   # Return the original text for now since we want to maintain formatting
+#   # for our final output
+#   text
+# end
 
 def has_repeated_letters?(word)
   # If a string's length is not equal to the count of unique
--- spec/repeating_letters_logic_spec.rb
+++ spec/repeating_letters_logic_spec.rb
@@ -3,20 +3,6 @@ require 'pry'
 require 'stringio'
 
 
-describe "get_file_name" do
-  describe "valid input" do
-    before do
-      $stdin = StringIO.new("text.txt\n")
-    end
-    after do
-      $stdin = STDIN
-    end
-    it "should return the stripped string of a valid file input" do
-      expect(get_file_name).to be == "text.txt"
-    end
-  end
-end
-
 describe "run_repeating_letters - wrapper method, contained logic tested separately" do
   it "should return the correct answer when given a valid file name" do
     word = run_repeating_letters('spec/test_files/valid.txt')
@@ -29,85 +15,85 @@ describe "run_repeating_letters - wrapper method, contained logic tested separat
     }.to output("No such file.\n").to_stdout
   end
 end
-
-describe "open_file" do
-  it "should return a File object for a valid file path" do
-    expect( open_file('spec/test_files/valid.txt').class ).to eq(File)
-  end
-  
-  it "should rescue an invalid file path and output an error message" do
-    expect{ 
-      open_file('spec/test_files/invalid.txt') 
-    }.to output("No such file.\n").to_stdout
-  end
-end
-
-describe "read_text_from_file" do
-  it "should return the text of a valid file" do
-    file = open_file('spec/test_files/valid.txt')
-    expect( read_text_from_file(file) ).to eq "Example text file."
-  end
-  
-  it "should abort if the text file is empty" do
-    file = open_file('spec/test_files/empty.txt')
-    begin
-      expect( read_text_from_file(file) ).should raise_error SystemExit
-    rescue SystemExit
-    end  
-  end
-  
-  it "should abort if the text file contains less than two viable characters" do
-    file = open_file('spec/test_files/too_short.txt')
-    begin
-      expect( read_text_from_file(file) ).should raise_error SystemExit
-    rescue SystemExit
-    end
-  end
-end
-
-describe "get_winning_word" do
-  it "should return the word with the most frequent repeating letter" do
-    text = "Romeo, Romeo, wherefore art thou Romeo?"
-    expect( get_winning_word(text) ).to eq("wherefore")
-  end
-  
-  it "should return the first occuring word in the event of a tie" do
-    text = "what's that? What's THAT!??"
-    expect( get_winning_word(text) ).to eq("that?")
-  end
-  
-  it "should ignore punctuation" do
-    text = "break-n-fix break-n-fix zoom"
-    expect( get_winning_word(text) ).to eq("zoom")
-  end
-  
-  it "should consider upper and lower case letter the same" do
-    text = "fOod Food"
-    expect( get_winning_word(text) ).to eq("fOod")
-  end
-  
-  it "should return the correct word with original formatting and punctuation" do
-    text = "It's a bright and s-U-n-Nn'!y day"
-    expect( get_winning_word(text) ).to eq("s-U-n-Nn'!y")
-  end
-end
-
-describe "has_repeated_letters?" do
-  it "should return true for a string with repeated letters" do
-    expect( has_repeated_letters?("woods") ).to eq(true)
-  end
-  
-  it "should return false for a string with no repeated letters" do
-    expect( has_repeated_letters?("taken") ).to eq(false)
-  end
-end
-
-describe "score_word" do
-  it "should return 3 for input 'ghooost'" do
-    expect( score_word("ghooost") ).to eq(3)
-  end
-  
-  it "should return 4 for input 'bananagrams'" do
-    expect( score_word("bananagrams") ).to eq(4)
-  end
-end
+# 
+# describe "open_file" do
+#   it "should return a File object for a valid file path" do
+#     expect( open_file('spec/test_files/valid.txt').class ).to eq(File)
+#   end
+#   
+#   it "should rescue an invalid file path and output an error message" do
+#     expect{ 
+#       open_file('spec/test_files/invalid.txt') 
+#     }.to output("No such file.\n").to_stdout
+#   end
+# end
+# 
+# describe "read_text_from_file" do
+#   it "should return the text of a valid file" do
+#     file = open_file('spec/test_files/valid.txt')
+#     expect( read_text_from_file(file) ).to eq "Example text file."
+#   end
+#   
+#   it "should abort if the text file is empty" do
+#     file = open_file('spec/test_files/empty.txt')
+#     begin
+#       expect( read_text_from_file(file) ).should raise_error SystemExit
+#     rescue SystemExit
+#     end  
+#   end
+#   
+#   it "should abort if the text file contains less than two viable characters" do
+#     file = open_file('spec/test_files/too_short.txt')
+#     begin
+#       expect( read_text_from_file(file) ).should raise_error SystemExit
+#     rescue SystemExit
+#     end
+#   end
+# end
+# 
+# describe "get_winning_word" do
+#   it "should return the word with the most frequent repeating letter" do
+#     text = "Romeo, Romeo, wherefore art thou Romeo?"
+#     expect( get_winning_word(text) ).to eq("wherefore")
+#   end
+#   
+#   it "should return the first occuring word in the event of a tie" do
+#     text = "what's that? What's THAT!??"
+#     expect( get_winning_word(text) ).to eq("that?")
+#   end
+#   
+#   it "should ignore punctuation" do
+#     text = "break-n-fix break-n-fix zoom"
+#     expect( get_winning_word(text) ).to eq("zoom")
+#   end
+#   
+#   it "should consider upper and lower case letter the same" do
+#     text = "fOod Food"
+#     expect( get_winning_word(text) ).to eq("fOod")
+#   end
+#   
+#   it "should return the correct word with original formatting and punctuation" do
+#     text = "It's a bright and s-U-n-Nn'!y day"
+#     expect( get_winning_word(text) ).to eq("s-U-n-Nn'!y")
+#   end
+# end
+# 
+# describe "has_repeated_letters?" do
+#   it "should return true for a string with repeated letters" do
+#     expect( has_repeated_letters?("woods") ).to eq(true)
+#   end
+#   
+#   it "should return false for a string with no repeated letters" do
+#     expect( has_repeated_letters?("taken") ).to eq(false)
+#   end
+# end
+# 
+# describe "score_word" do
+#   it "should return 3 for input 'ghooost'" do
+#     expect( score_word("ghooost") ).to eq(3)
+#   end
+#   
+#   it "should return 4 for input 'bananagrams'" do
+#     expect( score_word("bananagrams") ).to eq(4)
+#   end
+# end
