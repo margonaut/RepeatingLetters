aed60a7ac624d454768912ccf5c53c3f830a989f
Margaret Morgan <mmorgan@datto.com>
2016-06-30 14:07:27 -0400

    Added test for ties

--- spec/test_files_spec.rb
+++ spec/test_files_spec.rb
@@ -17,6 +17,11 @@ describe "Test File Inputs" do
     expect( word ).to eq("zoom")
   end
   
+  it "should return the first qualifying winner word in the event of a tie" do
+    word = run_repeating_letters("spec/test_files/tie.txt")
+    expect( word ).to eq("hello!")
+  end
+  
   it "should treat upper and lower case equaly and return 'foOd' from capitalization.txt" do
     word = run_repeating_letters("spec/test_files/capitalization.txt")
     expect( word ).to eq("foOd")
