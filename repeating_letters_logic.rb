2d21440643271da92ded315d8d20e129d11dbaa9
Margaret Morgan <mmorgan@datto.com>
2016-06-30 14:04:42 -0400

    Third draft working at a basic level, some tests passing

--- repeating_letters_logic.rb
+++ repeating_letters_logic.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Users will be prompted for a file path when the program starts
 def get_file_name
   puts "Enter the path of the .txt file you wish to scan"
@@ -19,28 +20,35 @@ end
 
 def run_repeating_letters(filename)
   winning_word = nil
-  winning_score = 0
+  winning_score = 1
   
-  
-  File.open('filename', 'r') do |file|
-    # let's process the text as we load each line
-    # this way we don't have to persist potentially huge collections of data
-    # for use elsewhere
-    file.each_line do |line|
-      # each line is made up of words
-      line.split(' ').each do |word|
-        # if the winning_score is greater than our word length, we know it
-        # has no chance of being the new winner, and move on
-        next if winning_score > word.length
-        score = score_word(word)
-        if score > winning_score
-          winning_word = word
-          winning_score = score
+  begin
+    File.open(filename, 'r') do |file|
+      # let's process the text as we load each line
+      # this way we don't have to persist potentially huge collections of data
+      # for use elsewhere
+      file.each_line do |line|
+        line.split(' ').each do |word|
+          # if the winning_score is greater than our word length, we know it
+          # has no chance of being the new winner, and move on
+          next if winning_score > word.length
+          score = score_word(word)
+          if score > winning_score
+            winning_word = word
+            winning_score = score
+          end
         end
       end
     end
+    if winning_word
+      puts winning_word
+      winning_word
+    else
+      puts "Your file contains no valid words."
+    end
+  rescue Errno::ENOENT => e
+    puts "No such file."
   end
-  winning_word
 end
 
 def open_file(filename)
@@ -156,7 +164,7 @@ def score_word(word)
   # Split up our word into an array of characters so
   # we can use emuberables methods that are unavailable to strings
   letters = word.downcase.gsub(/[^a-z0-9\s]/i, '').split('')
-  
+    
   # We're going to pass a new Hash with a default value of zero
   # into .each_with_object, which will iterate over our letters array
   # and return a hash containing a character frequency count
@@ -168,3 +176,7 @@ def score_word(word)
   score = letter_frequency.max_by { |k,v| v }[1]
 end
 
+# empty file
+# too short
+# ties
+# no such file
